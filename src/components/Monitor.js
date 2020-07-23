0a9dd5a673e21ba0d6dbe0cdf1e808be67485b78
PaoloCatalano <paolo12587@gmail.com>
2020-07-23 16:32:57 +0200

    commodore64

--- src/components/Monitor.js
+++ src/components/Monitor.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Monitor = () => {
     return (
+<section className="pane">
+        <div className="monitor-border">
         <div className="monitor">
-            c: | 
+            <p> **** COMMODORE 64 BASIC V2 **** </p>
+            <p>64K RAM SYSTEM 38911 BASIC BYTES FREE</p>
+            <p className="ready-monitor">READY.</p>
+            <p className="pointer-monitor">■</p>
         </div>
+
+        </div>
+
+</section>
     )
 }
 
